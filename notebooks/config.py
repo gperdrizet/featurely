5326880bbac61fcb0ce223b7c6aa4a68fbf2e69c
George Perdrizet <george@perdrizet.org>
2026-07-01 20:23:13 +0000

    Added color pallet

--- notebooks/config.py
+++ notebooks/config.py
@@ -17,3 +17,11 @@ DATA_DIR.mkdir(parents=True, exist_ok=True)
 OUTLIER_METHOD = "clip"  # Options: 'impute', 'clip', 'transform', 'ignore'
 OUTLIER_THRESHOLD = 2.25
 LOG_FEATURES = ["AveRooms", "AveOccup"]
+
+PIPELINE_COLORS = {
+    'raw':            '#aaaaaa',
+    '+ p_censored':   '#ffc000',
+    '+ cleaned':      '#5b9bd5',
+    '+ transforms':   '#70ad47',
+    '+ interactions': '#7030a0',
+}
