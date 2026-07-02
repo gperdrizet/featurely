540ad46c033436841421b1b460edff365ea9284a
George Perdrizet <george@perdrizet.org>
2026-07-02 03:46:49 +0000

    Updated color scheme

--- notebooks/config.py
+++ notebooks/config.py
@@ -21,10 +21,37 @@ OUTLIER_METHOD = "clip"  # Options: 'impute', 'clip', 'transform', 'ignore'
 OUTLIER_THRESHOLD = 2.25
 LOG_FEATURES = ["AveRooms", "AveOccup"]
 
+# Location encoding (notebook 06)
+GEOHASH_PRECISION = 4
+GEOHASH_MIN_CELL_COUNT = 100
+
+# Bin aggregates (notebook 07)
+AGG_BIN_FEATURES = ["Latitude", "MedInc"]
+AGG_N_BINS = 10
+AGG_STATS = ("mean",)
+
+# Clustering (notebook 08)
+CLUSTER_GEO_FEATURES = ["Latitude", "Longitude"]
+CLUSTER_GEO_K = 8
+CLUSTER_SOCIO_FEATURES = ["MedInc", "AveRooms", "AveOccup"]
+CLUSTER_SOCIO_K = 5
+
+# Spatial smoothing (notebook 09)
+SMOOTH_FEATURES = ["MedInc", "HouseAge", "AveRooms", "AveOccup", "Population"]
+SMOOTH_N_NEIGHBORS = 50
+
+# Polynomial expansion and PCA (notebook 10)
+POLY_DEGREE = 2
+
 PIPELINE_COLORS = {
     'raw':            '#aaaaaa',
     '+ p_censored':   '#ffc000',
     '+ cleaned':      '#5b9bd5',
     '+ transforms':   '#70ad47',
     '+ interactions': '#7030a0',
+    '+ location':     '#ed7d31',
+    '+ aggregates':   '#255e91',
+    '+ clusters':     '#9e480e',
+    '+ smoothed':     '#43682b',
+    '+ poly_pca':     '#c00000',
 }
